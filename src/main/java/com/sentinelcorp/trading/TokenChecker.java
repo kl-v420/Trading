4b330d8947f12cc4694711e9ec7db91726f417b9
NanakaKutsuki <NanakaKutsuki@gmail.com>
2023-07-10 14:40:50 -0400

    Fixing TradeRest and busted commit

--- src/main/java/com/sentinelcorp/trading/TokenChecker.java
+++ src/main/java/com/sentinelcorp/trading/TokenChecker.java
@@ -8,63 +8,52 @@ import com.sentinelcorp.trading.model.Account;
 import com.sentinelcorp.trading.model.Token;
 
 public class TokenChecker {
-	private static final Map<String, Token> TOKEN_MAP = new HashMap<String, Token>();
+    private static final Map<String, Token> TOKEN_MAP = new HashMap<String, Token>();
 
-	public static void addToken(String token, Account account, boolean login) {
-		Token coin = new Token();
-		coin.setLogin(login);
-		coin.setAccount(account);
-		coin.setToken(token);
-		coin.setExpire(LocalDateTime.now().plusMinutes(30));
-		TOKEN_MAP.put(token, coin);
+    public static void addToken(String token, Account account, boolean login) {
+	Token coin = new Token();
+	coin.setLogin(login);
+	coin.setAccount(account);
+	coin.setToken(token);
+	coin.setExpire(LocalDateTime.now().plusMinutes(30));
+	TOKEN_MAP.put(token, coin);
+    }
+
+    public static void deleteToken() {
+	for (String key : TOKEN_MAP.keySet()) {
+	    Token coin = TOKEN_MAP.get(key);
+	    if (LocalDateTime.now().isAfter(coin.getExpire())) {
+		TOKEN_MAP.remove(key);
+	    }
 	}
+    }
 
-	public static void deleteToken() {
-		for (String key : TOKEN_MAP.keySet()) {
-			Token coin = TOKEN_MAP.get(key);
-			if (LocalDateTime.now().isAfter(coin.getExpire())) {
-				TOKEN_MAP.remove(key);
-			}
-		}
+    public static Account changePassToken(String key) {
+	Token coin = TOKEN_MAP.get(key);
+	Account account = null;
+
+	if (coin != null && LocalDateTime.now().isBefore(coin.getExpire()) && !coin.isLogin()) {
+	    account = coin.getAccount();
 	}
+	return account;
+    }
 
-	public static Account changePassToken(String key) {
-		Token coin = TOKEN_MAP.get(key);
-		Account account = null;
-
-		if (coin != null && LocalDateTime.now().isBefore(coin.getExpire()) && !coin.isLogin()) {
-			account = coin.getAccount();
-		}
-		return account;
+    public static boolean logout(String key) {
+	boolean success = false;
+	Token token = TOKEN_MAP.remove(key);
+	if (token != null) {
+	    success = true;
 	}
+	return success;
+    }
 
-	public static boolean logout(String key) {
-		boolean success = false;
-		Token token = TOKEN_MAP.remove(key);
-		if (token != null) {
-			success = true;
-		}
-		return success;
+    public static Account verifyToken(String key) {
+	Token coin = TOKEN_MAP.get(key);
+	Account account = null;
+
+	if (coin != null && LocalDateTime.now().isBefore(coin.getExpire()) && coin.isLogin()) {
+	    account = coin.getAccount();
 	}
-
-	public static Account verifyToken(String key) {
-		Token coin = TOKEN_MAP.get(key);
-		Account account = null;
-
-		if (coin != null && LocalDateTime.now().isBefore(coin.getExpire()) && coin.isLogin()) {
-			account = coin.getAccount();
-		}
-		return account;
-	}
-
-	public static Token getByAccountId(int id) {
-		Token token = null;
-		for (Token t : TOKEN_MAP.values()) {
-			if (t.getAccount().getId() == id) {
-				token = t;
-			}
-		}
-		return token;
-
-	}
+	return account;
+    }
 }
