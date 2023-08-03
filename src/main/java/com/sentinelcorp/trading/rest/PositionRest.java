8b3fe7140e3d1c6af804b949f8f3f24ba37c7e2a
Kyle <Kyle@PC-of-1616>
2023-08-03 13:40:31 -0700

    Stock selling pt2 (fixing)

--- src/main/java/com/sentinelcorp/trading/rest/PositionRest.java
+++ src/main/java/com/sentinelcorp/trading/rest/PositionRest.java
@@ -1,5 +1,7 @@
 package com.sentinelcorp.trading.rest;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sentinelcorp.trading.TokenChecker;
 import com.sentinelcorp.trading.model.Account;
+import com.sentinelcorp.trading.model.Order;
 import com.sentinelcorp.trading.model.Position;
 import com.sentinelcorp.trading.repository.PositionsRepository;
 
@@ -25,6 +28,19 @@ public class PositionRest {
 			p = positionsRepo.findAllByAccountId(account.getId());
 		}
 		return p;
+	}
+
+	public Position getStockByAccId(BigDecimal price, Account account, Order order) {
+		Position position = new Position();
+		BigDecimal newQuan = BigDecimal.valueOf(order.getNumShares());
+		BigDecimal orderTotal = price.multiply(newQuan);
+		BigDecimal newQuan2 = BigDecimal.valueOf(position.getQuantity());
+		BigDecimal positionTotal = position.getPrice().multiply(newQuan2);
+		BigDecimal total = orderTotal.add(positionTotal);
+		BigDecimal avgPrice = total.divide(newQuan.add(newQuan2), 4, RoundingMode.HALF_UP);
+		position.setPrice(avgPrice);
+		position.setQuantity(position.getQuantity() + order.getNumShares());
+
 	}
 
 }
